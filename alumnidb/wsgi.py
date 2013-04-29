52e954634341f7985a49c372520ebba34bc3c2ec
Taylor Savage <taylor@taylorsavage.com>
2013-04-29 02:48:35 -0700

    Updated wsgi to serve

--- alumnidb/wsgi.py
+++ alumnidb/wsgi.py
@@ -21,7 +21,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 print >> sys.stderr, sys.path
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alumnidb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alumnidb.prod_settings")
 
 ALLDIRS = ['/home/sse/roster/website/venv/lib/python2.7/site-packages']
 
