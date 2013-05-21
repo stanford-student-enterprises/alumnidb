29887bd3210863bcb073901c0bb3a168c43356b7
Taylor Savage <taylor@taylorsavage.com>
2013-05-21 15:54:17 -0700

    Allows filtering not by year

--- alumnidb/database/forms.py
+++ alumnidb/database/forms.py
@@ -4,5 +4,5 @@ from django.conf import settings
 class FilterForm(forms.Form):
     is_current = forms.BooleanField(required=False)
     is_not_current = forms.BooleanField(required=False)
-    start_year = forms.ChoiceField(choices=settings.YEAR_CHOICES, required=False)
-    end_year = forms.ChoiceField(choices=settings.YEAR_CHOICES, required=False)
+    start_year = forms.ChoiceField(choices=[('', '-----')] + (settings.YEAR_CHOICES), required=False)
+    end_year = forms.ChoiceField(choices=[('', '-----')] + (settings.YEAR_CHOICES), required=False)
